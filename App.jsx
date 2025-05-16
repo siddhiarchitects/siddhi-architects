0edd6a4d752a08b05b48d9c839bb0c435f41c07a
siddhiarchitects <siddhiarchitects2025@gmail.com>
2025-05-16 21:54:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomePage from "./HomePage"; // adjust path if HomePage is in a different folder
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
