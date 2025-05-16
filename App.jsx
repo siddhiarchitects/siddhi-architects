0a49b7840a0dca81c7f5dcaa26b26d51ad4cb590
siddhiarchitects <siddhiarchitects2025@gmail.com>
2025-05-16 22:03:22 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,23 @@
-import React from "react";
-import HomePage from "./HomePage"; // adjust path if HomePage is in a different folder
+// App.jsx
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import ModularKitchens from './ModularKitchens';
+import Wardrobes from './Wardrobes';
+// Import other components accordingly
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/modular-kitchens" element={<ModularKitchens />} />
+        <Route path="/wardrobes" element={<Wardrobes />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
