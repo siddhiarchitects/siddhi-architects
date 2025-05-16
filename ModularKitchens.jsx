4d3baa56322c3e06264f5d1d0af31475e295330f
siddhiarchitects <siddhiarchitects2025@gmail.com>
2025-05-16 22:04:11 +0530

    Add files via upload

---
+++ ModularKitchens.jsx
@@ -0,0 +1,14 @@
+// ModularKitchens.jsx
+
+import React from 'react';
+
+const ModularKitchens = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">Modular Kitchens</h1>
+      {/* Add your modular kitchen images and content here */}
+    </div>
+  );
+};
+
+export default ModularKitchens;
